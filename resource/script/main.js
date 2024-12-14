266e825da4d09ab13923db1b469dc1c1172248cc
Nusrat Zarin Khan <62154689+zarin9863@users.noreply.github.com>
2024-12-14 23:24:48 +0600

    collect cart data for obj

--- resource/script/main.js
+++ resource/script/main.js
@@ -5,6 +5,9 @@ const increment   = document.getElementById("increment");
 const decrement   = document.getElementById("decrement");
 const showPrice   = document.getElementById("showPrice");
 const selectSize  = document.getElementById("selectSize");
+const productTitle= document.getElementById("productTitle").innerText;
+const orderContainer = [];
+const orderObj = {title:productTitle,price:"",color:"",size:"",qnt:"",preview:""};
 const imgContainer=[
     {
         color:"purple",
@@ -28,10 +31,12 @@ const imgContainer=[
 selectColor.addEventListener("click",(items)=>{
     if(items.target.classList.contains("h-4")){
         const takeId = items.target.parentElement.id;
+        orderObj.color = takeId;
 
         colorBaseProduct(takeId);
     }else{
         const takeId = items.target.id;
+        orderObj.color = takeId;
 
         colorBaseProduct(takeId)
     }
@@ -56,10 +61,12 @@ function colorBaseProduct(items){
     const step1 = imgContainer.filter(selection=>selection.color == items);
 
     if(imgContainer.filter(check=>check.color.includes(items)).length === 1){
-        productView.src = step1[0].source
+        productView.src = step1[0].source;
     }else{
         productView.src = productView.src;
     }
+
+    orderObj.preview = productView.src;
 }
 
 // decrement order
@@ -73,17 +80,30 @@ function countCurrentOrder(value){
     const currentOrder = parseInt(countOrder.innerText) + parseInt(value) < 0 ? 0 : parseInt(countOrder.innerText) + parseInt(value);
 
     countOrder.innerText = currentOrder;
+
+    orderObj.qnt = countOrder.innerText;
 }
 
 // show price base on size
 selectSize.addEventListener("click",(items)=>{
     const step1 = (items.target.innerText).match(/\d+/g);
     const step2 = Number(step1).toFixed(2);
+    // const step3 = (items.target.innerText).replace(/[^a-zA-Z\s]/g, "");
 
+    // get size code
+    if(items.target.classList.contains("px-[18px]")){
+        orderObj.size = (items.target.innerText).replace(/[^a-zA-Z\s]/g, "");
+    }else{
+        orderObj.size = (items.target.parentElement.innerText).replace(/[^a-zA-Z\s]/g, "");
+    }
     if(step2 == "NaN"){
-        showPrice.innerText = showPrice.innerText
+        showPrice.innerText = showPrice.innerText;
+
+        orderObj.price = showPrice.innerText;
     }else{
         showPrice.innerText = "$" + step2;
+
+        orderObj.price = step2;
     }
 
     const assume1 = items.target.parentElement.closest("div #selectSize");
@@ -96,4 +116,7 @@ selectSize.addEventListener("click",(items)=>{
     items.target.closest("div").classList.add("border-[#6576FF]");
     items.target.closest("div").childNodes[1].classList.add("text-[#6576FF]")
 })
-
+// add to cart
+function addCart(){
+   
+}
